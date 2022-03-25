bf774809b05a09d5e2457c161537b9753e2e6611
yoshiken621 <100831953+yoshiken621@users.noreply.github.com>
2022-03-25 17:54:55 +0900

    未ログイン時の動作変更

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except:[:index]
+  before_action :authenticate_user!, except:[:index, :show]
   before_action :set_action, only:[:show, :edit, :update]
 
   def index
