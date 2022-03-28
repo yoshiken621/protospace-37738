e83dafd00ccbb5c015e7acf6ddf72f7f975657b1
yoshiken621 <100831953+yoshiken621@users.noreply.github.com>
2022-03-28 11:18:58 +0900

    dependentオプションの追加

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,7 +2,7 @@ class Prototype < ApplicationRecord
   belongs_to :user
   # has_one_attached :image
   has_one_attached :image
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
 
   validates :title, presence: true
